e9978fcc2cb746f375dc7888674541939851ec9f
Richard Gajda <richardgajda11@gmail.com>
2023-10-14 13:29:23 -0400

    Sign up done!

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-import firebase from 'firebase'
-require('firebase/auth');
+const firebase = require('firebase/app');
+const { getAuth, createUserWithEmailAndPassword } = require("firebase/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname));
@@ -16,21 +16,27 @@ const firebaseConfig = {
     appId: "1:623467438376:web:16b2aaf94b95098621f01d",
     measurementId: "G-443SD60H0V"
   };
-
 firebase.initializeApp(firebaseConfig);
-
+const auth = getAuth();
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/index.html');
 });
 
-app.post('/login', (req, res) => {
+app.post('/signup', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    firebase.auth().signInWithEmailAndPassword(email, password)
+    createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             const user = userCredential.user;
-            res.send(`Login Successful for ${user.email}`);
+            user.providerData.forEach((profile) => {
+                console.log("Sign-in provider: " + profile.providerId);
+                console.log("  Provider-specific UID: " + profile.uid);
+                console.log("  Name: " + profile.displayName);
+                console.log("  Email: " + profile.email);
+                console.log("  Photo URL: " + profile.photoURL);
+              });
+            res.status(200).send("Logged");
         })
         .catch((error) => {
             const errorCode = error.code;
