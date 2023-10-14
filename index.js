c55d755b241f8833532a1d9a1d475b11039b4920
Richard Gajda <richardgajda11@gmail.com>
2023-10-14 14:32:33 -0400

    Modified location tracker

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-
-
 const successCallback = (position) => {
     const number = position.coords.longitude;
-
-    // Extract the sign (negative or positive)
     const sign = number < 0 ? '-' : '+';
-
-    // Convert the number to a string
     const numberString = Math.abs(number).toString();
-
-    // Extract the first five digits
     const firstFiveDigits = numberString.substring(0, 6);
-
-    // Concatenate the sign and the first five digits
     const result = sign + firstFiveDigits;
-
-    console.log(result); // Output: "-73.55"
+    console.log(result)
+    return(result);
 };
 
 const errorCallback = (error) => {
