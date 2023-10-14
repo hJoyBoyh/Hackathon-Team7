6132309c7a63f7914aaa5a95e83f3e1b8cf927d0
Richard Gajda <richardgajda11@gmail.com>
2023-10-14 15:12:37 -0400

    Added gitignore

--- server.js
+++ server.js
@@ -1,75 +1,80 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const firebase = require('firebase/app');
-const { getAuth, createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword } = require("firebase/auth");
+const bodyParser = require("body-parser");
+const firebase = require("firebase/app");
+const {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signInWithEmailAndPassword,
+} = require("firebase/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname));
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAmnb2L3FMQ750ov2XmN2xhM9rcrgRywCc",
-    authDomain: "bell-1b138.firebaseapp.com",
-    projectId: "bell-1b138",
-    storageBucket: "bell-1b138.appspot.com",
-    messagingSenderId: "623467438376",
-    appId: "1:623467438376:web:16b2aaf94b95098621f01d",
-    measurementId: "G-443SD60H0V"
-  };
+  apiKey: "AIzaSyAmnb2L3FMQ750ov2XmN2xhM9rcrgRywCc",
+  authDomain: "bell-1b138.firebaseapp.com",
+  projectId: "bell-1b138",
+  storageBucket: "bell-1b138.appspot.com",
+  messagingSenderId: "623467438376",
+  appId: "1:623467438376:web:16b2aaf94b95098621f01d",
+  measurementId: "G-443SD60H0V",
+};
 firebase.initializeApp(firebaseConfig);
 const auth = getAuth();
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/index.html");
 });
 
-app.post('/signup', (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    const fullName = req.body.fullName;
+app.post("/signup", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const fullName = req.body.fullName;
 
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-
-            updateProfile(user, {
-                displayName: fullName
-              }).then(() => {
-                res.status(200).send("Logged and updated");
-              }).catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.error(errorCode, errorMessage);
-                res.status(401).send(`Update Failed: ${errorMessage}`);
-              });
-              
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.error(errorCode, errorMessage);
-            res.status(401).send(`Login Failed: ${errorMessage}`);
-        });
-});
-
-app.post('/signin', (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-
-    signInWithEmailAndPassword(auth, email, password)
+  createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
-      res.status(200).send("Logged and updated", user);
+
+      updateProfile(user, {
+        displayName: fullName,
+      })
+        .then(() => {
+          res.status(200).send("Logged and updated");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.error(errorCode, errorMessage);
+          res.status(401).send(`Update Failed: ${errorMessage}`);
+        });
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       console.error(errorCode, errorMessage);
-    res.status(401).send(`Login Failed: ${errorMessage}`);
+      res.status(401).send(`Login Failed: ${errorMessage}`);
     });
-  
+});
+
+app.post("/signin", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      res.status(200).send("Singed in");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error(errorCode, errorMessage);
+      res.status(401).send(`Login Failed: ${errorMessage}`);
+    });
 });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
